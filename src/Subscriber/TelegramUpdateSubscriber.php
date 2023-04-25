54154ed68375aed8a6db1e38100640afba066a68
robin <robin.portner@protonmail.com>
2023-04-25 23:30:39 +0200

    test subscriber

---
+++ src/Subscriber/TelegramUpdateSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Subscriber;
+
+use BoShurik\TelegramBotBundle\Event\UpdateEvent;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use TelegramBot\Api\BotApi;
+
+class TelegramUpdateSubscriber implements EventSubscriberInterface
+{
+
+    public function __construct(
+        private LoggerInterface $logger,
+        private BotApi $bot,
+    )
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            UpdateEvent::class => 'onUpdate',
+        ];
+    }
+
+    public function onUpdate(UpdateEvent $event): void
+    {
+        $chatId = $event->getUpdate()->getMessage()->getChat()->getId();
+        $this->logger->debug('Chat ID: ' . $chatId);
+        $this->bot->sendMessage($chatId, 'Hello!');
+    }
+
+}
