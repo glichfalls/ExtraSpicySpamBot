49209cf3e7a05c4594acda8ced84587914436e9f
robin <robin.portner@protonmail.com>
2023-04-26 23:33:29 +0200

    added doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     BoShurik\TelegramBotBundle\BoShurikTelegramBotBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
