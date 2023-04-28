64c8bd41a82c6579bb7c7731d3a6ee24bb967c98
robin <robin.portner@protonmail.com>
2023-04-28 19:58:23 +0200

    added twig

--- config/bundles.php
+++ config/bundles.php
@@ -5,7 +5,7 @@ return [
     BoShurik\TelegramBotBundle\BoShurikTelegramBotBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
