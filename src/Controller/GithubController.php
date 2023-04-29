6dc16aed5dd9bf217e32199ad6943832c7855904
robin <robin.portner@protonmail.com>
2023-04-29 22:02:17 +0200

    added github webhook handling

--- src/Controller/GithubController.php
+++ src/Controller/GithubController.php
@@ -2,17 +2,58 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class GithubController
+class GithubController extends AbstractController
 {
 
-    #[Route('/github/webhook', methods: ['POST'])]
-    public function webhook(Request $request): void
+    public function __construct(private string $githubWebhookSecret)
     {
-        // run cache clear
-        exec('php bin/console cache:clear');
+
+    }
+
+    #[Route('/github/webhook', methods: ['POST'])]
+    public function webhook(Request $request): Response
+    {
+        $this->validate($request);
+
+        $data = json_decode($request->getContent(), true);
+
+        if ($data['action'] === 'completed') {
+            $application = new Application($this->container->get('kernel'));
+            $application->setAutoExit(false);
+            $application->run(
+                new ArrayInput(['command' => 'cache:clear']),
+                new NullOutput()
+            );
+        }
+
+        return new Response('ok', 200);
+    }
+
+    private function validate(Request $request)
+    {
+        $signatureHeader = $request->headers->get('x-hub-signature');
+
+        if ($signatureHeader === null) {
+            throw new \RuntimeException('Missing signature header');
+        }
+
+        [$algo, $signature] = explode('=', $signatureHeader, 2) + ['', ''];
+
+        if (!in_array($algo, hash_algos(), true)) {
+            throw new \RuntimeException('Invalid signature algorithm');
+        }
+
+        if (!hash_equals($signature, hash_hmac($algo, $request->getContent(), $this->githubWebhookSecret))) {
+            throw new \RuntimeException('Invalid signature');
+        }
     }
 
 }
