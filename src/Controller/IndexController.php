6f5e63ec80b1894608ecedfad831447c9a22970d
robin <robin.portner@protonmail.com>
2023-04-29 11:22:09 +0200

    test

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -14,6 +14,7 @@ class IndexController extends AbstractController
     public function index(LoggerInterface $logger): Response
     {
         $logger->info('test');
+        $logger->debug('handlers', $logger->getHandlers());
         return $this->render('pages/index.html.twig');
     }
 
