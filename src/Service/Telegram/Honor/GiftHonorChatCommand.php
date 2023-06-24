40bac47c9884a36e784229ed5a294baff9f1a5d5
robin <robin.portner@protonmail.com>
2023-06-24 20:30:13 +0200

    added gift honor command

---
+++ src/Service/Telegram/Honor/GiftHonorChatCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Service\Telegram\Honor;
+
+use App\Entity\Honor\HonorFactory;
+use App\Entity\Message\Message;
+use App\Repository\HonorRepository;
+use App\Service\Telegram\AbstractTelegramChatCommand;
+use App\Service\Telegram\TelegramService;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use TelegramBot\Api\Types\Update;
+
+class GiftHonorChatCommand extends AbstractTelegramChatCommand
+{
+
+    public function __construct(
+        EntityManagerInterface $manager,
+        TranslatorInterface $translator,
+        LoggerInterface $logger,
+        TelegramService $telegramService,
+        private HonorRepository $honorRepository,
+    )
+    {
+        parent::__construct($manager, $translator, $logger, $telegramService);
+    }
+
+    public function matches(Update $update, Message $message, array &$matches): bool
+    {
+        return preg_match('/^gift (?<amount>\d+)\s*ehre\s*@(?<name>.+)$/i', $message->getMessage(), $matches) === 1;
+    }
+
+    public function handle(Update $update, Message $message, array $matches): void
+    {
+        $amount = (int) $matches['amount'];
+
+        foreach ($this->telegramService->getUsersFromMentions($update) as $recipient) {
+
+            if ($recipient === null) {
+                $this->telegramService->replyTo($message,
+                    $this->translator->trans('telegram.honor.userNotFound', ['name' => $matches['name']])
+                );
+                continue;
+            }
+
+            $senderHonorAmount = $this->honorRepository->getHonorCount($message->getUser(), $message->getChat());
+
+            if ($senderHonorAmount < $amount) {
+                $this->telegramService->replyTo($message, sprintf('not enough honor, to gift %d honor to %s', $senderHonorAmount, $recipient->getFirstName()));
+                continue;
+            }
+
+            $this->manager->persist(HonorFactory::create($message->getChat(), null, $message->getUser(), -$amount));
+            $this->manager->persist(HonorFactory::create($message->getChat(), $message->getUser(), $recipient, $amount));
+            $this->manager->flush();
+            $this->telegramService->replyTo($message, sprintf('you have gifted %d honor to %s', $amount, $recipient->getFirstName()));
+        }
+    }
+
+}
