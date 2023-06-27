87b40dda11dbcd9da620a76ef5b4d9edf43bdb60
robin <robin.portner@protonmail.com>
2023-06-28 00:30:24 +0200

    migration

---
+++ migrations/Version20230627223006.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230627223006 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE upgrade_type ADD code VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_F828103777153098 ON upgrade_type (code)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_F828103777153098 ON upgrade_type');
+        $this->addSql('ALTER TABLE upgrade_type DROP code');
+    }
+}
