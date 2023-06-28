cb6176293d3a950ef6ea5d0f89656358d71ea47e
robin <robin.portner@protonmail.com>
2023-06-28 23:52:41 +0200

    formatting

--- src/Service/Telegram/Stocks/BuyStockChatCommand.php
+++ src/Service/Telegram/Stocks/BuyStockChatCommand.php
@@ -24,11 +24,11 @@ class BuyStockChatCommand extends AbstractStockChatCommand
             $portfolio = $this->getPortfolioByMessage($message);
             $transaction = $this->buyStock($portfolio, $symbol, $amount);
             $this->telegramService->replyTo($message, sprintf(
-                "You bought\n<strong>%s</strong>\n%dx %s\n%s\nStock Price: <code>$%.2f</code> (%d Ehre)\nTotal buy Price: %d Ehre",
+                "<strong>%s</strong>\n%s\n\n%dx %s\n: <code>$%.2f</code> (%d Ehre)\nTotal buy Price: %d Ehre",
                 $transaction->getPrice()->getStock()->getName(),
+                $transaction->getPrice()->getStock()->getType(),
                 $transaction->getAmount(),
                 $transaction->getPrice()->getStock()->getDisplaySymbol(),
-                $transaction->getPrice()->getStock()->getType(),
                 $transaction->getPrice()->getPrice(),
                 $transaction->getPrice()->getHonorPrice(),
                 $transaction->getHonorTotal(),
