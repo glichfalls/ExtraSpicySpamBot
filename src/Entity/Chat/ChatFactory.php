16b227bd5067946ac69beaf5f8b42960cce05cca
robin <robin.portner@protonmail.com>
2023-07-15 03:11:42 +0200

    changed chat factory

--- src/Entity/Chat/ChatFactory.php
+++ src/Entity/Chat/ChatFactory.php
@@ -11,22 +11,19 @@ class ChatFactory
     public static function createFromUpdate(Update $update): Chat
     {
         $telegramChat = $update->getMessage()->getChat();
-        $chat = new Chat();
-        $chat->setChatId($telegramChat->getId());
-        if ($telegramChat->getType() === 'private') {
-            $chat->setName($telegramChat->getUsername());
-        } else {
-            $chat->setName($telegramChat->getTitle());
-        }
-        $chat->setCreatedAt(new \DateTime());
-        $chat->setUpdatedAt(new \DateTime());
-        return $chat;
+        return self::extracted($telegramChat);
     }
 
     public static function createFromMessage(Message $message): Chat
     {
         $telegramChat = $message->getChat();
+        return self::extracted($telegramChat);
+    }
+
+    private static function extracted(\TelegramBot\Api\Types\Chat $telegramChat): Chat
+    {
         $chat = new Chat();
+        $chat->setConfig(new ChatConfig());
         $chat->setChatId($telegramChat->getId());
         if ($telegramChat->getType() === 'private') {
             $chat->setName($telegramChat->getUsername());
