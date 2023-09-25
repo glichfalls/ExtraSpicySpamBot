390abaf9d2686082c479ccf2ef1bf5238c28419d
robin <robin.portner@protonmail.com>
2023-09-25 22:39:02 +0200

    fixed class

--- src/Service/Telegram/Honor/Collectables/Trade/ShowCollectableInfoChatCommand.php
+++ src/Service/Telegram/Honor/Collectables/Trade/ShowCollectableInfoChatCommand.php
@@ -5,25 +5,14 @@ namespace App\Service\Telegram\Honor\Collectables\Trade;
 use App\Entity\Chat\Chat;
 use App\Entity\Collectable\CollectableItemInstance;
 use App\Entity\User\User;
-use App\Service\Telegram\AbstractTelegramCallbackQuery;
-use App\Service\Telegram\Collectables\CollectableService;
-use App\Service\Telegram\TelegramCallbackQueryListener;
-use App\Service\Telegram\TelegramService;
+use App\Service\Telegram\Honor\Collectables\AbstractCollectableTelegramCallbackQuery;
 use App\Utils\NumberFormat;
 use TelegramBot\Api\Types\Inline\InlineKeyboardMarkup;
-use TelegramBot\Api\Types\ReplyKeyboardMarkup;
 use TelegramBot\Api\Types\Update;
 
-class ShowCollectableInfoChatCommand extends AbstractTelegramCallbackQuery
+class ShowCollectableInfoChatCommand extends AbstractCollectableTelegramCallbackQuery
 {
-
     public const CALLBACK_KEYWORD = 'collectable:show';
-
-    public function __construct(
-        private TelegramService $telegram,
-        private CollectableService $collectableService,
-    ) {
-    }
 
     public function getCallbackKeyword(): string
     {
@@ -36,7 +25,7 @@ class ShowCollectableInfoChatCommand extends AbstractTelegramCallbackQuery
         $collectableId = array_pop($data);
         $collectable = $this->collectableService->getInstanceById($collectableId);
         if ($collectable === null) {
-            $this->telegram->answerCallbackQuery($update->getCallbackQuery(), 'Collectable not found.', true);
+            $this->telegramService->answerCallbackQuery($update->getCallbackQuery(), 'Collectable not found.', true);
             return;
         }
         $text = <<<TEXT
@@ -52,7 +41,7 @@ class ShowCollectableInfoChatCommand extends AbstractTelegramCallbackQuery
         );
         if ($collectable->getCollectable()->getImagePublicPath() !== null) {
             $fullPath = sprintf('https://%s/%s', $_SERVER['HTTP_HOST'], $collectable->getCollectable()->getImagePublicPath());
-            $this->telegram->sendImage(
+            $this->telegramService->sendImage(
                 $chat->getChatId(),
                 $fullPath,
                 caption: $message,
@@ -60,14 +49,14 @@ class ShowCollectableInfoChatCommand extends AbstractTelegramCallbackQuery
                 replyMarkup: $this->getKeyboard($collectable),
             );
         } else {
-            $this->telegram->sendText(
+            $this->telegramService->sendText(
                 $chat->getChatId(),
                 $message,
                 threadId: $update->getCallbackQuery()->getMessage()->getMessageThreadId(),
                 replyMarkup: $this->getKeyboard($collectable),
             );
         }
-        $this->telegram->answerCallbackQuery($update->getCallbackQuery());
+        $this->telegramService->answerCallbackQuery($update->getCallbackQuery());
     }
 
     private function getKeyboard(CollectableItemInstance $collectable): ?InlineKeyboardMarkup
