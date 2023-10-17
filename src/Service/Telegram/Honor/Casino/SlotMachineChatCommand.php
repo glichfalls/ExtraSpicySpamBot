cd7542c5b4e64a29527678bc7aab84d83c1aba7f
Robin Portner <robin.portner@jkweb.ch>
2023-10-17 15:09:55 +0200

    changed slot machine

--- src/Service/Telegram/Honor/Casino/SlotMachineChatCommand.php
+++ src/Service/Telegram/Honor/Casino/SlotMachineChatCommand.php
@@ -2,18 +2,22 @@
 
 namespace App\Service\Telegram\Honor\Casino;
 
+use App\Entity\Chat\Chat;
 use App\Entity\Message\Message;
+use App\Entity\User\User;
 use App\Service\HonorService;
 use App\Service\Telegram\AbstractTelegramChatCommand;
+use App\Service\Telegram\TelegramCallbackQueryListener;
 use App\Service\Telegram\TelegramService;
 use App\Utils\NumberFormat;
 use App\Utils\Random;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use TelegramBot\Api\Types\Inline\InlineKeyboardMarkup;
 use TelegramBot\Api\Types\Update;
 
-class SlotMachineChatCommand extends AbstractTelegramChatCommand
+class SlotMachineChatCommand extends AbstractTelegramChatCommand implements TelegramCallbackQueryListener
 {
 
     private const PRICE = 10_000;
@@ -36,71 +40,23 @@ class SlotMachineChatCommand extends AbstractTelegramChatCommand
 
     public function handle(Update $update, Message $message, array $matches): void
     {
-        $currentHonor = $this->honorService->getCurrentHonorAmount($message->getChat(), $message->getUser());
-        if ($currentHonor < self::PRICE) {
-            $this->telegramService->replyTo($message, 'not enough Ehre');
-            return;
-        }
         $jackpot = $this->honorService->getSlotMachineJackpot($message->getChat());
-        $this->honorService->removeHonor($message->getChat(), $message->getUser(), self::PRICE);
-        $runs = 1;
-        if (isset($matches['amount'])) {
-            $amount = NumberFormat::getIntValue($matches['amount']);
-            if ($amount > 5) {
-                $this->telegramService->replyTo($message, 'you can only run the slot machine 5 times at once');
-                return;
-            }
-            if ($amount > 0) {
-                $runs = $amount;
-            }
-        }
-        $cost = $runs * self::PRICE;
-        if ($cost > $currentHonor) {
-            $this->telegramService->replyTo($message, sprintf('you need %s Ehre to run the slot machine %s times', NumberFormat::format($cost), $runs));
-            return;
-        }
-        $losses = 0;
-        $result = [];
-        for ($i = 0; $i < $runs; $i++) {
-            $result = $this->run();
-            if ($result === [7,7,7]) {
-                $amount = $jackpot->getAmount();
-                $this->honorService->addHonor($message->getChat(), $message->getUser(), $amount);
-                $text = <<<TEXT
-                🎰 777 🎰
-                
-                JACKPOT
-                after %s tries
-                you win %s Ehre
-                TEXT;
-                $totalTries = $losses + 1;
-                $this->honorService->removeHonor($message->getChat(), $message->getUser(), $totalTries * self::PRICE);
-                $this->telegramService->replyTo($message, sprintf($text, $totalTries, NumberFormat::format($amount)));
-                $jackpot->setAmount(0);
-                $this->manager->flush();
-                return;
-            } else {
-                $losses++;
-                $jackpot->setAmount($jackpot->getAmount() + self::PRICE);
-            }
-        }
-        $this->honorService->removeHonor($message->getChat(), $message->getUser(), $losses * self::PRICE);
-        $this->manager->flush();
-        if ($runs === 1) {
-            $text = <<<TEXT
-            🎰 %s 🎰
-            
-            you lose
-            new jackpot: %s Ehre
-            TEXT;
-            $this->telegramService->replyTo($message, sprintf($text, implode(' ', $result), NumberFormat::format($jackpot->getAmount())));
-        } else {
-            $text = <<<TEXT
-            you lost %s times in a row (%s Ehre)
-            new jackpot: %s Ehre
-            TEXT;
-            $this->telegramService->replyTo($message, sprintf($text, $losses, NumberFormat::format($losses * self::PRICE), NumberFormat::format($jackpot->getAmount())));
-        }
+        $text = <<<TEXT
+        🎰 SLOT MACHINE 🎰
+        
+        JACKPOT: %s Ehre
+        COST: %s Ehre
+        TEXT;
+        $this->telegramService->sendText(
+            $message->getChat()->getChatId(),
+            sprintf(
+                $text,
+                NumberFormat::format($jackpot->getAmount()),
+                NumberFormat::format(self::PRICE),
+            ),
+            threadId: $message->getTelegramThreadId(),
+            replyMarkup: $this->getKeyboard(),
+        );
     }
 
     /**
@@ -114,6 +70,57 @@ class SlotMachineChatCommand extends AbstractTelegramChatCommand
             Random::arrayElement($options),
             Random::arrayElement($options),
         ];
+    }
+
+    public function getCallbackKeyword(): string
+    {
+        return 'slot';
+    }
+
+    public function handleCallback(Update $update, Chat $chat, User $user): void
+    {
+        $callbackQuery = $update->getCallbackQuery();
+        $currentHonor = $this->honorService->getCurrentHonorAmount($chat, $user);
+        if ($currentHonor < self::PRICE) {
+            $this->telegramService->answerCallbackQuery($callbackQuery, 'not enough Ehre');
+            return;
+        }
+        $jackpot = $this->honorService->getSlotMachineJackpot($chat);
+        $this->honorService->removeHonor($chat, $user, self::PRICE);
+        $result = $this->run();
+        if ($result === [7,7,7]) {
+            $amount = $jackpot->getAmount();
+            $this->honorService->addHonor($chat, $user, $amount);
+            $text = <<<TEXT
+                🎰 777 🎰
+                
+                JACKPOT
+                @%s wins %s Ehre
+                TEXT;
+            $this->honorService->removeHonor($chat, $user, self::PRICE);
+            $this->telegramService->sendText($chat->getChatId(), sprintf($text, $user->getName(), NumberFormat::format($amount)));
+            $jackpot->setAmount(0);
+            $this->manager->flush();
+            return;
+        }
+        $this->manager->flush();
+        $this->telegramService->answerCallbackQuery(
+            $callbackQuery,
+            sprintf(
+                '%s you lose %s Ehre',
+                implode(' ', $result),
+                NumberFormat::format(self::PRICE)
+            ),
+        );
+    }
+
+    public function getKeyboard(): InlineKeyboardMarkup
+    {
+        return new InlineKeyboardMarkup([
+            [
+                ['text' => 'play', 'callback_data' => 'slot'],
+            ],
+        ]);
     }
 
     public function getDescription(): string
