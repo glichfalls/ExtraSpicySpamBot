eddf2a7c2c7d79686fbd8e395c7476b8b9be36c4
Robin Portner <robin.portner@protonmail.com>
2023-11-18 00:14:32 +0100

    migration

---
+++ migrations/Version20231117231337.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231117231337 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE item_effect ADD amount INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE item_effect DROP amount');
+    }
+}
