87bee3b8a7ca54dbbbbecedea3ecac59524bc4cb
Robin Portner <robin.portner@protonmail.com>
2023-11-17 23:00:28 +0100

    changed syntax

--- src/Service/Telegram/General/DebugChatCommand.php
+++ src/Service/Telegram/General/DebugChatCommand.php
@@ -11,7 +11,7 @@ class DebugChatCommand extends AbstractTelegramChatCommand
 
     public function matches(Update $update, Message $message, array &$matches): bool
     {
-        return preg_match('/!debug (on|off)/i', $message->getMessage(), $matches) === 1;
+        return preg_match('/!debug/i', $message->getMessage(), $matches) === 1;
     }
 
     public function handle(Update $update, Message $message, array $matches): void
